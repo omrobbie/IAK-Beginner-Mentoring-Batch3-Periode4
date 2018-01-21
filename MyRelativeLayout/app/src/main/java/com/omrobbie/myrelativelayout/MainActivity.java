bc224ccc9c58f8a02ccb9ef09c59b07e284f8f8d
omrobbie <putu.roby@yahoo.com>
2018-01-21 14:42:04 +0800

    buat home activity dengan view warna-warni

--- MyRelativeLayout/app/src/main/java/com/omrobbie/myrelativelayout/MainActivity.java
+++ MyRelativeLayout/app/src/main/java/com/omrobbie/myrelativelayout/MainActivity.java
@@ -1,5 +1,6 @@
 package com.omrobbie.myrelativelayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -43,6 +44,10 @@ public class MainActivity extends AppCompatActivity {
         } else {
             if (uname.equals("admin") && upass.equals("123")) {
                 Toast.makeText(this, "Anda berhasil login!", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
             }
         }
     }
