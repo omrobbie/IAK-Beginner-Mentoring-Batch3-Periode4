3f5abc74d0f16fb7673b2c17091c5bf6bf71b7cb
omrobbie <putu.roby@yahoo.com>
2018-01-21 12:20:07 +0800

    koneksikan activity dengan layout

--- MyLinearLayout/app/src/main/java/com/omrobbie/mylinearlayout/MainActivity.java
+++ MyLinearLayout/app/src/main/java/com/omrobbie/mylinearlayout/MainActivity.java
@@ -1,12 +1,19 @@
 package com.omrobbie.mylinearlayout;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    private EditText etUsername, etPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +21,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.i(TAG, "onCreate: ");
+
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String uname = etUsername.getText().toString();
+                String upass = etPassword.getText().toString();
+                boolean isError = false;
+
+                if (uname.isEmpty()) {
+                    isError = true;
+                    etUsername.setError("Username masih kosong!");
+                }
+
+                if (upass.isEmpty()) {
+                    isError = true;
+                    etPassword.setError("Password masih kosong!");
+                }
+
+                if (isError) {
+                    Toast.makeText(MainActivity.this, "Data belum lengkap!", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (uname.equals("admin") && upass.equals("123")) {
+                        Toast.makeText(MainActivity.this, "Anda berhasil login!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Username dan Password belum benar!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 
     @Override
