9b897ca133d42648ac42b83fa7729234d91162c2
omrobbie <putu.roby@yahoo.com>
2018-02-04 17:12:22 +0800

    buat logout

--- MyLoginScreen/app/src/main/java/com/omrobbie/myloginscreen/DetailActivity.java
+++ MyLoginScreen/app/src/main/java/com/omrobbie/myloginscreen/DetailActivity.java
@@ -3,6 +3,8 @@ package com.omrobbie.myloginscreen;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,6 +20,7 @@ public class DetailActivity extends AppCompatActivity {
     TextView tv_username;
 
     SharedPreferences sharedPreferences;
+    SharedPreferences.Editor login_data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,7 @@ public class DetailActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         sharedPreferences = getSharedPreferences("login_data", MODE_PRIVATE);
+        login_data = sharedPreferences.edit();
 
         String prefCheck = sharedPreferences.getString("dataOn", null);
         String prefUsername = sharedPreferences.getString("username", null);
@@ -33,5 +37,23 @@ public class DetailActivity extends AppCompatActivity {
         if (prefCheck != null) {
             tv_username.setText(prefUsername);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mn_logout:
+                login_data.clear();
+                login_data.apply();
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
