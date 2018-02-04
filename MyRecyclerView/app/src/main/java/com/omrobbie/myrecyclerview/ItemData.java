67d53d04013e6bc05bff9672647f7b75400361d3
omrobbie <putu.roby@yahoo.com>
2018-02-04 13:28:00 +0800

    buat file pojo untuk menyimpan data item

---
+++ MyRecyclerView/app/src/main/java/com/omrobbie/myrecyclerview/ItemData.java
@@ -0,0 +1,36 @@
+package com.omrobbie.myrecyclerview;
+
+/**
+ * Created by omrobbie on 04/02/2018.
+ */
+
+public class ItemData {
+
+    private int avatar;
+    private String judul;
+    private String content;
+
+    public int getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(int avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
