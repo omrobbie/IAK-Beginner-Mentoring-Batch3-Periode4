68b64f98be27db9aad1e9f8bdc047b8201cbf097
omrobbie <putu.roby@yahoo.com>
2018-02-04 14:14:24 +0800

    buat item adapter

---
+++ MyRecyclerView/app/src/main/java/com/omrobbie/myrecyclerview/ItemAdapter.java
@@ -0,0 +1,72 @@
+package com.omrobbie.myrecyclerview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * Created by omrobbie on 04/02/2018.
+ */
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
+
+    private List<ItemData> itemData;
+
+    public ItemAdapter(List<ItemData> itemData) {
+        this.itemData = itemData;
+    }
+
+    public void addData(ItemData itemData) {
+        this.itemData.add(itemData);
+        notifyDataSetChanged();
+    }
+
+    public void replaceData(List<ItemData> itemData) {
+        this.itemData = itemData;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_list, parent, false)
+        );
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.bind(itemData.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemData.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private CircleImageView iv_avatar;
+        private TextView tv_judul;
+        private TextView tv_content;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            iv_avatar = (CircleImageView) itemView.findViewById(R.id.iv_avatar);
+            tv_judul = (TextView) itemView.findViewById(R.id.tv_judul);
+            tv_content = (TextView) itemView.findViewById(R.id.tv_content);
+        }
+
+        public void bind(ItemData itemData) {
+            iv_avatar.setImageResource(itemData.getAvatar());
+            tv_judul.setText(itemData.getJudul());
+            tv_content.setText(itemData.getContent());
+        }
+    }
+}
--- MyRecyclerView/app/src/main/java/com/omrobbie/myrecyclerview/MainActivity.java
+++ MyRecyclerView/app/src/main/java/com/omrobbie/myrecyclerview/MainActivity.java
@@ -1,13 +1,29 @@
 package com.omrobbie.myrecyclerview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rv_list;
+
+    private List<ItemData> itemData = new ArrayList<>();
+    private ItemAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        adapter = new ItemAdapter(itemData);
+
+        rv_list = (RecyclerView) findViewById(R.id.rv_list);
+        rv_list.setLayoutManager(new LinearLayoutManager(this));
+        rv_list.setAdapter(adapter);
     }
 }
