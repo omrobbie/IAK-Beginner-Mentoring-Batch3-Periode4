8e261c7728ab48694ec636cbce107fbe024f0d11
omrobbie <putu.roby@yahoo.com>
2018-02-04 15:30:37 +0800

    setup option menu

--- MyRecyclerView/app/src/main/java/com/omrobbie/myrecyclerview/MainActivity.java
+++ MyRecyclerView/app/src/main/java/com/omrobbie/myrecyclerview/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,8 +34,28 @@ public class MainActivity extends AppCompatActivity {
         loadDummyData();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(this, "Anda mengklik tombol " + item.toString(), Toast.LENGTH_SHORT).show();
+
+        switch (item.getItemId()) {
+            case R.id.mn_1:
+                break;
+
+            case R.id.mn_2:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
     private void loadDummyData() {
-        for (int i= 1; i<10; i++) {
+        for (int i = 1; i < 10; i++) {
             itemData.add(new ItemData(R.drawable.iak, "Data " + String.valueOf(i), "Content data " + String.valueOf(i)));
         }
     }
